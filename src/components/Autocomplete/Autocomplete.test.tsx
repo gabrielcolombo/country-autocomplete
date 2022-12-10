188c90a048678fd415411e84d2083f6c563c6604
Gabriel Colombo <gcolombo07@gmail.com>
2022-12-10 14:34:16 -0300

    feat: Scaffolds autocomplete component file structure

---
+++ src/components/Autocomplete/Autocomplete.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Autocomplete from './index';
+
+test('renders autocomplete component', () => {
+  render(<Autocomplete />);
+
+  // expect(element).toBeInTheDocument();
+});
---
+++ src/components/Autocomplete/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Autocomplete.css';
+
+const Autocomplete = () => {
+  return (
+    <div>
+      <input />
+
+      <ul></ul>
+    </div>
+  )
+}
+
+export default Autocomplete;
