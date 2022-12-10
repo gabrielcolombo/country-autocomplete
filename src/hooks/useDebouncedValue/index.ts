82cc2e2fd2ed434f299e233c230e718a2d81652f
Gabriel Colombo <gcolombo07@gmail.com>
2022-12-10 15:15:11 -0300

    feat: Implements useDebouncedValue hook

---
+++ src/hooks/useDebouncedValue/index.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useDebouncedValue = (value: string, delayInMs: number = 250) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  const handleValueChange = () => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delayInMs);
+
+    return () => clearTimeout(timeout);
+  }
+
+  useEffect(handleValueChange, [value, delayInMs]);
+
+  return debouncedValue;
+}
+
+export default useDebouncedValue;
