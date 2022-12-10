c36777b88fea2f7621f310a2530f0c631b22ea63
Gabriel Colombo <gcolombo07@gmail.com>
2022-12-10 14:54:45 -0300

    feat: Outlines autocomplete component structure with aria attributes

--- src/components/Autocomplete/Autocomplete.test.tsx
+++ src/components/Autocomplete/Autocomplete.test.tsx
@@ -2,8 +2,27 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Autocomplete from './index';
 
-test('renders autocomplete component', () => {
-  render(<Autocomplete />);
+/* eslint-disable */
+test('renders the autocomplete component', () => {
+  const { container } = render(<Autocomplete />);
 
-  // expect(element).toBeInTheDocument();
+  const element = container.querySelector('#Autocomplete');
+
+  expect(element).toBeInTheDocument();
+});
+
+test('renders the autocomplete input', () => {
+  const { container } = render(<Autocomplete />);
+
+  const input = container.querySelector('#Autocomplete__input');
+
+  expect(input).toBeInTheDocument();
+});
+
+test('renders the autocomplete dropdown', () => {
+  const { container } = render(<Autocomplete />);
+
+  const listbox = container.querySelector('#Autocomplete__listbox');
+
+  expect(listbox).toBeInTheDocument();
 });
--- src/components/Autocomplete/index.tsx
+++ src/components/Autocomplete/index.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Autocomplete.css';
 
-const Autocomplete = () => {
-  return (
-    <div>
-      <input />
+const Autocomplete = ({ ...props }) => {
+  const {
+    id = "Autocomplete",
+    label
+  } = props;
 
-      <ul></ul>
+  const [options, setOptions] = useState([{}, {}]);
+
+  return (
+    <div id={id}>
+      {label && (
+        <label htmlFor={`${id}__input`}>
+          {label}
+        </label>
+      )}
+
+      <input
+        id={`${id}__input`}
+        autoComplete="off"
+        role="combobox"
+        aria-expanded="false"
+        aria-autocomplete="list"
+        aria-controls={`${id}__listbox`}
+      />
+
+      <ul
+        id={`${id}__listbox`}
+        role="listbox"
+        {...(label) ? { "aria-label": label } : {}}
+      >
+        {options.map((option, index) => (
+          <li
+            role="option"
+            aria-posinset={index + 1}
+            aria-setsize={options.length}
+            aria-selected="true"
+            tabIndex={-1}
+            key={`${JSON.stringify(option)}__${index}`}
+          >
+            Option #{index}
+          </li>
+        ))}
+        
+      </ul>
     </div>
   )
 }
